dd2b67c5b3059fe57b725cc5f9273d8ca1603669
prganesan <45071481+prganesan@users.noreply.github.com>
2021-11-11 11:53:29 +0000

    Update test_kafka_to_elasticsearch_pipeline.py

--- test_kafka_to_elasticsearch_pipeline.py
+++ test_kafka_to_elasticsearch_pipeline.py
@@ -7,37 +7,65 @@ from subprocess import Popen, PIPE
 
 from kafka.admin import KafkaAdminClient
 from streamsets.testframework.utils import get_random_string
+from elasticsearch import Elasticsearch
 
 logger = logging.getLogger(__name__)
 #Generate a Random Topic Name and Job Name
-job_name = get_random_string()
+job_name = get_random_string(string.ascii_lowercase)
+#index_name='testempdetails1'
+index_name=job_name
+consumer_group_name=job_name
+topic_name=job_name
+SAMPLE_DATA1 = dict(FirstName='MAURICE', LastName='GARIN',EmpId=1)
+SAMPLE_DATA2 = dict(FirstName='LUCIEN', LastName='POTHIER',EmpId=2)
+SAMPLE_DATA3 = dict(FirstName='FERNAND', LastName='AUGEREAU', EmpId=3)
 
-SAMPLE_DATA = [dict(FirstName='MAURICE', LastName='GARIN',EmployeeId='1'),
-               dict(FirstName='LUCIEN', LastName='POTHIER',EmployeeId='2'),
-               dict(FirstName='FERNAND', LastName='AUGEREAU', EmployeeId='3')]
 
-        
 def test_complete(elasticsearch_data):
     """Test that first name and last name are split as expected."""
-    EXPECTED_NAMES = [dict(FirstName='Maurice', LastName='Garin' , EmployeeId='1'),
-                      dict(FirstName='Lucien', LastName='Pothier',EmployeeId='2'),
-                      dict(FirstName='Fernand', LastName='Augereau',EmployeeId='3')]
-    assert EXPECTED_NAMES == [{key: record[key] for key in ['FirstName', 'LastName', 'EmployeeId']}
+    EXPECTED_NAMES = [dict(FirstName='MAURICE', LastName='GARIN' , EmpId=1),
+                      dict(FirstName='LUCIEN', LastName='POTHIER',EmpId=2),
+                      dict(FirstName='FERNAND', LastName='AUGEREAU',EmpId=3)]
+    assert EXPECTED_NAMES == [{key: record[key] for key in ['FirstName', 'LastName', 'EmpId']}
                               for record in elasticsearch_data]
 
 
 @pytest.fixture(scope='module')
 def elasticsearch_data(sch, pipeline, cluster,elasticsearch):
-    
+
     """Send basic messages to Kafka"""
     producer = cluster.kafka.producer()
-    producer.send(job_name, SAMPLE_DATA)
+    producer.send(topic_name, json.dumps(SAMPLE_DATA1).encode('utf-8'))
+    producer.send(topic_name, json.dumps(SAMPLE_DATA2).encode('utf-8'))
+    producer.send(topic_name, json.dumps(SAMPLE_DATA3).encode('utf-8'))
     #logger.info('broker_configs %s ...',broker_configs)
-    runtime_params = {'Topic_Name': job_name,'Index_Name': job_name}
+    runtime_params = {'Topic_Name': topic_name,'Index_Name': index_name, 'Consumer_Group_Name': consumer_group_name}
     #admin_client = KafkaAdminClient(bootstrap_servers="node-1.cluster:9092", client_id='test',security_protocol='SASL_PLAINTEXT',sasl_mechanism='GSSAPI',sasl_kerberos_service_name='kafka')
     admin_client = KafkaAdminClient(bootstrap_servers="172.28.0.4:9092", client_id='test',security_protocol="PLAINTEXT")
+    es1 = Elasticsearch([{"host":"172.28.0.4","port":9200}])
+
 
     try:
+        mapping = '''
+        {
+           "mappings": {
+              "properties" : {
+                 "FirstName": {
+                   "type": "text"
+                  },
+                 "LastName": {
+                   "type": "text"
+                  },
+                "EmpId": {
+                   "type": "integer"
+                 }
+              }
+
+          }
+        }'''
+
+        es1.indices.create(index=index_name,body=mapping,ignore=400)
+        #time.sleep(5)
         logger.info('Creating test job ...')
         job_builder = sch.get_job_builder()
         logger.info('Creating test job ...')
@@ -53,7 +81,7 @@ def elasticsearch_data(sch, pipeline, cluster,elasticsearch):
         #Wait for records to be written.
         time.sleep(5)
 
-        data_in_elasticsearch = [hit['_source'] for hit in elasticsearch.client.search(index=index)]
+        data_in_elasticsearch = [hit['_source'] for hit in elasticsearch.client.search(index=index_name)]
         yield data_in_elasticsearch
     finally:
         #Stop Job
@@ -61,7 +89,7 @@ def elasticsearch_data(sch, pipeline, cluster,elasticsearch):
         #Delete Job
         sch.delete_job(job)
         logger.info('Deleting dummy data from %s ... ', job_name)
-        admin_client.delete_topics([job_name],1000)
-        
-        logger.info('Deleting Elasticsearch index %s ...', index)
-        elasticsearch.client.delete_index(index)
+        admin_client.delete_topics([topic_name],1000)
+
+        logger.info('Deleting Elasticsearch index %s ...', index_name)
+        elasticsearch.client.delete_index(index_name)
