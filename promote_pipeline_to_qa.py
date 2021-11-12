f206458b65dcac0939144fb2fa5096d860da9946
prganesan <45071481+prganesan@users.noreply.github.com>
2021-11-12 14:36:19 +0000

    Update promote_pipeline_to_qa.py

--- promote_pipeline_to_qa.py
+++ promote_pipeline_to_qa.py
@@ -21,11 +21,11 @@ dev_control_hub = ControlHub(server_url=args.dev_sch_url,username=args.dev_sch_u
 pipeline_list = dev_control_hub.pipelines.get_all(pipeline_id=args.pipeline_id)
 pipeline_export_data = dev_control_hub.export_pipelines(pipelines=pipeline_list)
 time.sleep(5)
-pipeline_export_data
-with open ('./sch_pipeline_exports.zip', 'wb') as output_file:
-    output_file.write(pipeline_export_data)
-qa_control_hub = ControlHub(server_url=args.qa_sch_url,username=args.qa_sch_user,password=args.qa_sch_password)
-with open('./sch_pipeline_exports.zip', 'rb') as input_file:
-    pipelines_zip_data = input_file.read()
-pipelines = qa_control_hub.import_pipelines_from_archive(archive=pipelines_zip_data,
-                                              commit_message='Exported as zip from sdc')
+#pipeline_export_data
+#with open ('./sch_pipeline_exports.zip', 'wb') as output_file:
+#    output_file.write(pipeline_export_data)
+#qa_control_hub = ControlHub(server_url=args.qa_sch_url,username=args.qa_sch_user,password=args.qa_sch_password)
+#with open('./sch_pipeline_exports.zip', 'rb') as input_file:
+#    pipelines_zip_data = input_file.read()
+#pipelines = qa_control_hub.import_pipelines_from_archive(archive=pipelines_zip_data,
+#                                              commit_message='Exported as zip from sdc')
