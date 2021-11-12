ff3d8ed8b5e55c548aa575a80ad2d66b31cbc848
prganesan <45071481+prganesan@users.noreply.github.com>
2021-11-12 13:38:52 +0000

    Create promote_pipeline_to_qa.py

---
+++ promote_pipeline_to_qa.py
@@ -0,0 +1,29 @@
+import sys
+import argparse
+from streamsets.sdk import ControlHub
+ControlHub.VERIFY_SSL_CERTIFICATES = False
+
+parser = argparse.ArgumentParser()
+parser.add_argument("--pipeline_id", help="Pipeline to be exported from Dev Environment")
+parser.add_argument("--dev_sch_url", help="Control Hub URL for Dev Environment")
+parser.add_argument("--dev_sch_user", help="Control Hub User for Dev Environment")
+parser.add_argument("--dev_sch_password", help="Control Hub Password for Dev Environment")
+parser.add_argument("--qa_pipeline_name", help="Pipeline Name for QA Environment")
+parser.add_argument("--qa_sch_url", help="Control Hub URL for QA Environment")
+parser.add_argument("--qa_sch_user", help="Control Hub User for QA Environment")
+parser.add_argument("--qa_sch_password", help="Control Hub Password for QA Environment")
+
+
+args = parser.parse_args()
+
+dev_control_hub = ControlHub(server_url=args.dev_sch_url,username=args.dev_sch_user,password=args.dev_sch_password)
+pipeline_list = dev_control_hub.pipelines.get_all(pipeline_id=args.pipeline_id)
+pipeline_export_data = dev_control_hub.export_pipelines(pipelines=pipeline_list)
+with open ('./sch_pipeline_exports.zip', 'wb') as output_file:
+    output_file.write(pipeline_export_data)
+qa_control_hub = ControlHub(server_url=args.qa_sch_url,username=args.qa_sch_user,password=args.qa_sch_password)
+with open('./sdc_exports_for_sch.zip', 'rb') as input_file:
+    pipelines_zip_data = input_file.read()
+pipelines = qa_control_hub.import_pipelines_from_archive(archive=pipelines_zip_data,
+											  pipeline_name=args.qa_pipeline_name,
+                                              commit_message='Exported as zip from sdc')
