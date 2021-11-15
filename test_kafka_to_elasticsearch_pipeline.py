6b9ef88accd33472d48f8ccfb9de1d001731fe75
prganesan <45071481+prganesan@users.noreply.github.com>
2021-11-15 19:54:16 +0000

    Update test_kafka_to_elasticsearch_pipeline.py

--- test_kafka_to_elasticsearch_pipeline.py
+++ test_kafka_to_elasticsearch_pipeline.py
@@ -12,21 +12,7 @@ from streamsets.testframework.utils import get_random_string
 from elasticsearch import Elasticsearch
 
 logger = logging.getLogger(__name__)
-parser = argparse.ArgumentParser()
 
-
-parser.add_argument("--sch-server-url", help="SCH URL")
-parser.add_argument("--sch-username", help="SCH Username")
-parser.add_argument("--sch-password", help="SCH Password")
-parser.add_argument("--pipeline_id", help="Pipeline Id")
-parser.add_argument("--elasticsearch-url", help="Elastic Search URL")
-parser.add_argument("--cluster-server", help="Kafka Cluster Broker URL")
-parser.add_argument("--kafka-version", help="Kafka Version")
-parser.add_argument("--kafka-zookeeper", help="Kafka Zookeeper")
-#parser.add_argument("--upgrade-jobs", help="Kafka Version")
-#parser.add_argument("--junit-xml", help="Junit")
-
-args = parser.parse_args()
 
 
 #Generate a Random Topic Name and Job Name
@@ -59,9 +45,9 @@ def elasticsearch_data(sch, pipeline, cluster,elasticsearch):
     logger.info('broker_configs %s ...',sys.argv)
     runtime_params = {'Topic_Name': topic_name,'Index_Name': index_name, 'Consumer_Group_Name': consumer_group_name}
     #admin_client = KafkaAdminClient(bootstrap_servers="172.28.0.4:9092", client_id='test',security_protocol="PLAINTEXT")
-    admin_client = KafkaAdminClient(bootstrap_servers=args.cluster-server, client_id='test',security_protocol="PLAINTEXT")
+    admin_client = KafkaAdminClient(bootstrap_servers=argsv[11].cluster-server, client_id='test',security_protocol="PLAINTEXT")
     #es1 = Elasticsearch([{"host":"172.28.0.4","port":9200}])
-    es1 = Elasticsearch([{"host":args.elasticsearch-url,"port":9200}])
+    es1 = Elasticsearch([{"host":argsv[9],"port":9200}])
 
     try:
         mapping = '''
