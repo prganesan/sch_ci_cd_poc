b4c8aee53400eecbb0a8a0c970460f17df462f35
prganesan <45071481+prganesan@users.noreply.github.com>
2021-11-17 19:56:31 +0000

    Update get_values_from_azure_vault.py

--- get_values_from_azure_vault.py
+++ get_values_from_azure_vault.py
@@ -10,10 +10,17 @@ client = SecretClient(vault_url=KVUri, credential=credential)
 
 dev-sch-user=client.get_secret('dev-sch-user')
 dev-sch-password=client.get_secret('dev-sch-password')
+dev-sch-url=client.get_secret('dev-sch-url')
+dev-sch-password=client.get_secret('dev-sch-password')
 qa-sch-user=client.get_secret('qa-sch-user')
 qa-sch-password=client.get_secret('qa-sch-password')
+qa-sch-url=client.get_secret('qa-sch-url')
 
-print ('##vso[task.setvariable variable=dev-sch-user]'+dev-sch-user)
-print ('##vso[task.setvariable variable=dev-sch-password]'+dev-sch-password)
-print ('##vso[task.setvariable variable=qa-sch-user]'+qa-sch-user)
-print ('##vso[task.setvariable variable=qa-sch-password]'+qa-sch-password)
+print ('##vso[task.setvariable variable=DEV_SCH_USER]'+dev-sch-user)
+print ('##vso[task.setvariable variable=DEV_SCH_PASSWORD]'+dev-sch-password)
+print ('##vso[task.setvariable variable=DEV_SCH_URL]'+dev-sch-url)
+
+print ('##vso[task.setvariable variable=QA_SCH_USER]'+qa-sch-user)
+print ('##vso[task.setvariable variable=QA_SCH_PASSWORD]'+qa-sch-password)
+print ('##vso[task.setvariable variable=QA_SCH_URL]'+qa-sch-url)
+
